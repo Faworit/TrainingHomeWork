db316c3ab692388187098f2bfb245b59a1192190
Faworit <sergei_ryabzev@mail.ru>
2021-06-02 17:56:25 +0600

    Revert "Merge branch 'security'"
    This reverts commit fbad3e218679a72ceb07f142ec3fcbc27b1a55a5, reversing
    changes made to 5de8f91a23deacdb53092ebc6c7dd1a63b2101c0.

--- freshnewsrest/src/main/java/com/epam/freshnewsrest/FreshnewsrestApplication.java
+++ freshnewsrest/src/main/java/com/epam/freshnewsrest/FreshnewsrestApplication.java
@@ -2,12 +2,8 @@ package com.epam.freshnewsrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
 public class FreshnewsrestApplication {
 
     public static void main(String[] args) {
--- freshnewsrest/src/main/java/com/epam/freshnewsrest/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.freshnewsrest.repository;
-
-import com.epam.freshnewsrest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}
