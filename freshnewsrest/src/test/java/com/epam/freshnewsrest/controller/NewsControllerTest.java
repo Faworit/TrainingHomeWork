c443cb4f6a6b18bdcdf8a6c71ce70886137c789e
Faworit <sergei_ryabzev@mail.ru>
2021-06-02 16:57:31 +0600

    Test git

---
+++ freshnewsrest/src/test/java/com/epam/freshnewsrest/controller/NewsControllerTest.java
@@ -0,0 +1,13 @@
+package com.epam.freshnewsrest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NewsControllerTest {
+
+    @Test
+    public void testPagination() {
+
+    }
+}
